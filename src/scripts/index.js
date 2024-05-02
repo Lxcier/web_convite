c3884a098843c1c6522b5b897dd3223c4c7edbda
Lxcier <dev.lxcier@gmail.com>
2024-05-02 04:31:24 -0300

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,35 @@
+// animações
+AOS.init();
+// confirmar presença
+function confirm () {
+    document.getElementById('confirm-message').innerHTML="Você confirmou a sua presença."
+}
+
+// contador / timer
+const eventTime = new Date ("Oct 05, 2024 17:30:00");
+const eventTimeStamp = eventTime.getTime();
+
+const timer = setInterval(function(){
+    const now = new Date();
+    const currentTimeStamp = now.getTime();
+
+    const timeUntilEvent = eventTimeStamp - currentTimeStamp;
+
+    const mounthInMS = 1000 * 60 * 60 * 24 * 30;
+    const dayInMS = 1000 * 60 * 60 * 24;
+    const hourInMS = 1000 * 60 * 60;
+    const minutesInMS = 1000 * 60;
+
+    const mounthsUntilEvent = Math.floor(timeUntilEvent / mounthInMS);
+    const daysUntilEvent = Math.floor((timeUntilEvent % mounthInMS) / dayInMS) ;
+    const hoursUntilEvent = Math.floor((timeUntilEvent % dayInMS) / hourInMS);
+    const minutesUntilEvent = Math.floor((timeUntilEvent % hourInMS) / minutesInMS);
+    const secondsUntilEvent = Math.floor((timeUntilEvent % minutesInMS) / 1000);
+
+    document.getElementById('timer').innerHTML = `${mounthsUntilEvent}m ${daysUntilEvent}d ${hoursUntilEvent}h ${minutesUntilEvent}m ${secondsUntilEvent}s`
+
+    if (timeUntilEvent < 0) {
+        clearInterval (timer);
+        document.getElementById('timer-text').innerHTML = 'Ish! Já era... Agora só ano que vem ^-^'
+    }
+})
